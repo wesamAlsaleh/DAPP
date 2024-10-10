1e29330e10dc5894b5b3c9c1ffb17c79b32cbd0d
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-10-10 16:10:42 +0300

    Add Android safe area view custom padding

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+// import android safe area view custom style
+import GlobalStyles from "@/scripts/GlobalStyles";
 
 export default function Home() {
   return (
-    <View>
+    <SafeAreaView style={GlobalStyles.droidSafeArea}>
       <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
