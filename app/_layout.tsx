2573bef94bd594ce40dccde97d930ba62458d791
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-10-10 16:02:36 +0300

    Refactor code structure and remove unused files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
