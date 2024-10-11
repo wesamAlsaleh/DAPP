3d2f028722c24a2a4c139f6428cbd6c7f2a037d4
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-10-11 16:26:56 +0300

    Refactor layout component and update function name

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 
-export default function AuthLayout() {
+export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- app/index.tsx
+++ app/index.tsx
@@ -4,3 +4,5 @@ export default function Index() {
   // this will always redirect to the welcome page
   return <Redirect href={"/(auth)/sign-in"} />;
 }
+
+// TODO: if the user is already logged in, redirect them to the home page else redirect them to the sign-in page
