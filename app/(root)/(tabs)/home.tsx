9b6b3459584aa4f5f4fed41a432dfd281688e5a2
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-10-11 17:05:37 +0300

    Refactor home component to use ProtectedRoute for authenticated access

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,16 +1,22 @@
 import React, { useContext } from "react";
 import { Text, View } from "react-native";
 
+// import the AuthContext through useAuth hook
 import { useAuth } from "@/contexts/AuthContext";
+
+// import the ProtectedRoute component to protect the route from unauthorized access
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function home() {
   // get the user data from the AuthContext
   const { user } = useAuth();
 
   return (
-    <View>
-      <Text className="text-2xl">Welcome home {user?.name}</Text>
-      <Text className="text-base">Home Screen is ....</Text>
-    </View>
+    <ProtectedRoute>
+      <View>
+        <Text className="text-2xl">Welcome home {user?.name}</Text>
+        <Text className="text-base">Home Screen is ....</Text>
+      </View>
+    </ProtectedRoute>
   );
 }
