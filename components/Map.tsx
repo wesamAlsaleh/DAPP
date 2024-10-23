e0f21aa4d1d97e9e877a7d106dfa0bbbfbd0d2f5
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-10-23 20:26:12 +0300

    Showed the google map successfully

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+// import the map staff
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
 export default function Map() {
   const googleApiUrl = process.env.EXPO_PUBLIC_GOOGLE_KEY;
 
   return (
-    <View>
-      <Text className="bg-emerald-600 text-base text-black rounded-md text-center">
-        MAP
-      </Text>
-    </View>
+    <MapView
+      provider={PROVIDER_DEFAULT} // Use Google Maps "default"
+      showsCompass={true} // Show the compass
+      className="w-full h-full"
+    />
   );
 }
